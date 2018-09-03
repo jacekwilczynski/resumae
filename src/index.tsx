14cfa33fdfb73d8d6ebc3cceca161190a090b2e5
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 22:56:06 +0200

    Get resume url from query string

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,18 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import 'styles/index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { parse } from 'query-string';
 
 ReactDOM.render(
   <Router>
     <Route
-      path="/render/:source?"
-      render={({ match }) => (
-        <App
-          resumeUrl={
-            match.params.source || `${process.env.PUBLIC_URL}/resume.yaml`
-          }
-        />
-      )}
+      path="/render"
+      render={({ location }) => {
+        const { resume } = parse(location.search);
+        return (
+          <App resumeUrl={resume || `${process.env.PUBLIC_URL}/resume.yaml`} />
+        );
+      }}
     />
   </Router>,
   document.getElementById('root')
