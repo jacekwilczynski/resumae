33bd89784006bc64c20aa1c067f5488d26a5b193
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 23:30:32 +0200

    Get rid of routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,9 @@ import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import 'styles/index.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { parse } from 'query-string';
 
 ReactDOM.render(
-  <Router>
-    <Route
-      path="/render"
-      render={({ location }) => {
-        const { resume } = parse(location.search);
-        return (
-          <App resumeUrl={resume || `${process.env.PUBLIC_URL}/resume.yaml`} />
-        );
-      }}
-    />
-  </Router>,
+  <App resumeUrl={`${process.env.PUBLIC_URL}/resume.yaml`} />,
   document.getElementById('root')
 );
 registerServiceWorker();
