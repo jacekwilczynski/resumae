3e4e95834c5795aef9c635b976e8be260d9cbefc
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 22:27:46 +0200

    Add /render route

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,21 @@ import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import 'styles/index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <App resumeUrl={`${process.env.PUBLIC_URL}/resume.yaml`} />,
+  <Router>
+    <Route
+      path="/render/:source?"
+      render={({ match }) => (
+        <App
+          resumeUrl={
+            match.params.source || `${process.env.PUBLIC_URL}/resume.yaml`
+          }
+        />
+      )}
+    />
+  </Router>,
   document.getElementById('root')
 );
 registerServiceWorker();
