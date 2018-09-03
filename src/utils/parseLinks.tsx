c13324d8c7b4330c1beefec863f711759f80e7bc
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 20:33:20 +0200

    Rename variable

--- src/utils/parseLinks.tsx
+++ src/utils/parseLinks.tsx
@@ -17,12 +17,12 @@ const parseLinks = (str: string): React.ReactElement<any> => {
   const anchorMatch = regExp.exec(str);
   if (anchorMatch) {
     const [, before, prefix, url, after] = anchorMatch;
-    const shortUrl = url.replace(/\/$/, '');
+    const trimmedUrl = url.replace(/\/$/, '');
     return (
       <React.Fragment>
         {parseLinks(before)}
         <a href={prefix + url} target="_blank" rel="noopener noreferrer">
-          {shortUrl}
+          {trimmedUrl}
         </a>
         {after}
       </React.Fragment>
