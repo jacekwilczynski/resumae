21541e0361cca2c61d69039e9be3a43c9de4c678
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 19:43:07 +0200

    Remove link detection logic from List component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,22 +1,15 @@
 import * as React from 'react';
 
 export interface ListProps {
-  items: string[]
+  items: string[];
 }
 
 const List: React.SFC<ListProps> = ({ items }) => (
   <ul className="list">
     {items.map(item => (
-      <li
-        key={item}
-        className="list__item"
-        dangerouslySetInnerHTML={{
-          __html: item.replace(
-            /(https?:\/\/)([\w/.-_]+)/g,
-            '<a href="$1$2" target="_blank" rel="noopener noreferrer">$2</a>'
-          )
-        }}
-      />
+      <li key={item} className="list__item">
+        {item}
+      </li>
     ))}
   </ul>
 );
