49efb08f1adde11600375460645789948ad1742b
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-03 20:00:28 +0200

    Use link parsing within Text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
+import ResumaeText from 'containers/ResumaeText';
 
-const Text: React.SFC = ({ children }) => <p className="text">{children}</p>;
+interface TextProps {
+  children: string;
+}
+
+const Text: React.SFC<TextProps> = ({ children }) => (
+  <p className="text">
+    <ResumaeText>{children}</ResumaeText>
+  </p>
+);
 
 export default Text;
