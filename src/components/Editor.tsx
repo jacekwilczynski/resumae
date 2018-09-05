7ca732712eeb4eb01bd11b272411c6ba6f986b90
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-05 11:04:50 +0200

    Refactor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MonacoEditor, { EditorDidMount } from 'react-monaco-editor';
-import calcEditorWidth from '../utils/calcEditorWidth';
+import calcEditorWidth from 'utils/calcEditorWidth';
 
 export interface EditorProps {
   visible: boolean;
@@ -9,32 +9,30 @@ export interface EditorProps {
   editorDidMount: EditorDidMount;
 }
 
-class Editor extends React.Component<EditorProps> {
-  render() {
-    const { visible, value, onChange, editorDidMount } = this.props;
-    return (
-      <div
-        className={
-          'resumae__editor' + (visible ? '' : ' resumae__editor--hidden')
-        }
-      >
-        <MonacoEditor
-          language="yaml"
-          theme="vs-dark"
-          height={window.innerHeight}
-          width={calcEditorWidth()}
-          value={value}
-          onChange={onChange}
-          options={{
-            wordWrap: 'bounded',
-            wrappingIndent: 'indent',
-            scrollBeyondLastLine: false
-          }}
-          editorDidMount={editorDidMount}
-        />
-      </div>
-    );
-  }
-}
+const Editor: React.SFC<EditorProps> = ({
+  visible,
+  value,
+  onChange,
+  editorDidMount
+}) => (
+  <div
+    className={'resumae__editor' + (visible ? '' : ' resumae__editor--hidden')}
+  >
+    <MonacoEditor
+      language="yaml"
+      theme="vs-dark"
+      height={window.innerHeight}
+      width={calcEditorWidth()}
+      value={value}
+      onChange={onChange}
+      options={{
+        wordWrap: 'bounded',
+        wrappingIndent: 'indent',
+        scrollBeyondLastLine: false
+      }}
+      editorDidMount={editorDidMount}
+    />
+  </div>
+);
 
 export default Editor;
