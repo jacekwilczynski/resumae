ec79ddc59c20bb9828ddab10675af893dd70ed5a
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-25 19:34:36 +0200

    Allow customizing image offset

--- src/components/resume/header/Photo.tsx
+++ src/components/resume/header/Photo.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 
 export interface PhotoProps {
   src: string;
+  position?: string;
   href?: string;
 }
 
-const Photo: React.SFC<PhotoProps> = ({ src, href }) => (
+const Photo: React.SFC<PhotoProps> = ({ src, position, href }) => (
   <a
     href={href}
     className="header__column header__photo"
-    style={{ backgroundImage: `url(${src})` }}
+    style={{
+      backgroundImage: `url(${src})`,
+      backgroundPosition: position
+    }}
     target="_blank"
     rel="noopener noreferrer"
   />
