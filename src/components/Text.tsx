dc7edaf0954829edecc5f23ca1768274aaeb0351
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-10-04 21:06:23 +0200

    Add multi-paragraph text support

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,13 +2,22 @@ import * as React from 'react';
 import TextWithCustomTags from 'containers/TextWithCustomTags';
 
 export interface TextProps {
-  children: string;
+  children: string | string[];
 }
 
-const Text: React.SFC<TextProps> = ({ children }) => (
-  <p className="text">
-    <TextWithCustomTags>{children}</TextWithCustomTags>
-  </p>
-);
+const Text: React.SFC<TextProps> = ({ children }) => {
+  const paragraphs = (Array.isArray(children)
+    ? children
+    : [children]) as string[];
+  return (
+    <React.Fragment>
+      {paragraphs.map(paragraph => (
+        <p className="text">
+          <TextWithCustomTags>{paragraph}</TextWithCustomTags>
+        </p>
+      ))}
+    </React.Fragment>
+  );
+};
 
 export default Text;
